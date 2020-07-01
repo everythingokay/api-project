9040fb037dc6800725d232ab98c070629d87d5e0
rose-gd-artist <61298013+rose-gd-artist@users.noreply.github.com>
2020-06-30 20:59:10 -0400

    push initial teacher's ajax code

--- index.js
+++ index.js
@@ -12,25 +12,82 @@
 //     })
 
 
-    $(document).ready(function (){
+// let searchBox = document.getElementById("searchBox");
+// let searchButton = document.getElementById("searchButton");
+// searchButton.addEventListener("click", function(){
+
+//     $(document).ready(function (){
   
 
-            $.ajax({
-                url: "https://restcountries.eu/rest/v2/all",
-                
-                success: function(response) {
-                    console.log(response)
-                    getInfo(response)
-                }
-            }) 
+//         $.ajax({
+//             url: "https://restcountries.eu/rest/v2/all",
+//             data: {s: searchBox.value},
+//             success: function(response) {
+//                 console.log(response)
+//                 getInfo(response)
+//             }
+//         }) 
+        
+//         function getInfo(){
 
+//             $("body") = response.name;
+
+//         }
+    
+//     })
+
+
+
+// })
+
+    // $(document).ready(function (){
+  
+
+    //         $.ajax({
+    //             url: "https://restcountries.eu/rest/v2/all",
+    //             data: {s: searchBox.value},
+    //             success: function(response) {
+    //                 console.log(response)
+    //                 getInfo(response)
+    //             }
+    //         }) 
+            
+    //         function getInfo(){
+
+    //             document.body.innerHTML = response.name;
+
+    //         }
+        
+    //     })
+
+
+        $(document).ready(function (){
+  
             let searchBox = document.getElementById("searchBox");
             let searchButton = document.getElementById("searchButton");
-            
+
+            searchButton.addEventListener("click", getInfo)
+
             function getInfo(){
 
-                document.body.innerHTML = response.name;
+                $.ajax({
+                    url: "https://restcountries.eu/rest/v2/name/" + searchBox.value,
+                    success: function(response) {
+                        console.log(response)
+                        displayCountryName(response)
+                    }
+                }) 
+
+
+                
 
             }
+
+            function displayCountryName(countries){
+                $("#country-name").html("Name of the country: " + countries[0].name)
+            }
+
+
+
         
         })
