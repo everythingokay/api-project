f6336cd1d78c5c6185f908d7b52bcaf001e290a3
rose-gd-artist <61298013+rose-gd-artist@users.noreply.github.com>
2020-07-07 17:15:31 -0400

    forgot to test it beforepush

--- index.js
+++ index.js
@@ -11,20 +11,31 @@
                     url: "https://restcountries.eu/rest/v2/name/" + searchBox.value,
                     success: function(response) {
                         console.log(response)
-                        displayCountryName(response)
+                        displayCountryInfo(response)
+                        displayFlag(response)
                     }
                 }) 
-
-
-                
-
             }
 
-            function displayCountryName(countries){
-                $("#country-name").html("Name of the country: " + countries[0].name)
-            }
+            
+            function displayCountryInfo(countries){
+                            $("#country-name").html("Name of the country: " + countries[0].name)
+                            $("#country-capital").html("Capital: " + countries[0].capital)
+                            $("#country-pop").html("Population: " + countries[0].population)
+                            $("#country-currency").html("Currency: " + countries[0].currencies[0].name)
+                        }
 
+            function displayFlag(countries){
+                let flagPic = document.createElement("div");
+                flagPic.style.width = "200px";
+                flagPic.style.height = "100px";
+                flagPic.style.display = "block";
+                flagPic.style.marginTop = "1vw";
+                flagPic.style.backgroundImage = "url(" + countries[0].flag + ")";
+                flagPic.style.backgroundSize = "100% 100%";
+                document.body.appendChild(flagPic);           
+            }
 
 
         
-        }
+        })
